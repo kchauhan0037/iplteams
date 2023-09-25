36dafa77ec4ebdf9dc0e0205adf9772467329851
karan chauhan <k.chauhan0037@gmail.com>
2023-09-25 17:15:54 +0530

    changes to id

--- teamdetails.js
+++ teamdetails.js
@@ -30,26 +30,61 @@ if (teamdatafromlocal[id]) {
     teamdetailscontainer.innerHTML = "<p>Team not found.</p>";
 }
 
-var allplayerscontainer=document.getElementById('allplayerscontainer')
+// var allplayerscontainer=document.getElementById('allplayerscontainer')
 
-for(var i=0;i<playeratafromlocal.length;i++){
+// for(var i=0;i<playeratafromlocal.length;i++){
 
-    if(teamdatafromlocal[id].topBatsman==playeratafromlocal[i].playerName || teamdatafromlocal[id].topBowler==playeratafromlocal[i].playerName || teamdatafromlocal[id].teamName==playeratafromlocal[i].playerfrom){
-        allplayerscontainer.innerHTML+=`<div class="allplayerscontainercard" id="player${playeratafromlocal[i].id}">
+//     if(teamdatafromlocal[id].topBatsman==playeratafromlocal[i].playerName || teamdatafromlocal[id].topBowler==playeratafromlocal[i].playerName || teamdatafromlocal[id].teamName==playeratafromlocal[i].playerfrom){
+//         allplayerscontainer.innerHTML+=`<div class="allplayerscontainercard" id="player${playeratafromlocal[i].id}">
 
-        <img src="${playeratafromlocal[i].player_photo_url}" class="playerimg" alt="">
-        <p>${playeratafromlocal[i].playerName}</p>
-        <p>${playeratafromlocal[i].playerprice}</p>
+//         <img src="${playeratafromlocal[i].player_photo_url}" class="playerimg" alt="">
+//         <p>${playeratafromlocal[i].playerName}</p>
+//         <p>${playeratafromlocal[i].playerprice}</p>
 
-    </div>`
+//     </div>`
+//     }
+// }
+
+// var allplayerscontainercard=document.getElementsByClassName('allplayerscontainercard')
+// for (var i = 0; i < allplayerscontainercard.length; i++) {
+//     allplayerscontainercard[i].addEventListener('click', function () {
+//         var playerId = this.getAttribute('id');
+//         var url = `playerdetails.html?playerId=${playerId}`;
+//         window.location.href = url;
+//     });
+// }
+
+var allplayerscontainer = document.getElementById('allplayerscontainer');
+
+for (var i = 0; i < playeratafromlocal.length; i++) {
+    if (
+        teamdatafromlocal[id].topBatsman == playeratafromlocal[i].playerName ||
+        teamdatafromlocal[id].topBowler == playeratafromlocal[i].playerName ||
+        teamdatafromlocal[id].teamName == playeratafromlocal[i].playerfrom
+    ) {
+        var playerId = `player${playeratafromlocal[i].id-1}`;
+
+        var playerCard = document.createElement('div');
+        playerCard.className = 'allplayerscontainercard';
+        playerCard.id = playerId;
+
+        playerCard.innerHTML = `
+            <img src="${playeratafromlocal[i].player_photo_url}" class="playerimg" alt="">
+            <p>${playeratafromlocal[i].playerName}</p>
+            <p>${playeratafromlocal[i].playerprice}</p>
+        `;
+
+        // Create a closure to capture the correct playerId
+        (function (playerId) {
+            playerCard.addEventListener('click', function () {
+                var url = `playerdetails.html?playerId=${playerId}`;
+                window.location.href = url;
+            });
+        })(playerId);
+
+        allplayerscontainer.appendChild(playerCard);
     }
 }
 
-var allplayerscontainercard=document.getElementsByClassName('allplayerscontainercard')
-for (var i = 0; i < allplayerscontainercard.length; i++) {
-    allplayerscontainercard[i].addEventListener('click', function () {
-        var playerId = this.getAttribute('id');
-        var url = `playerdetails.html?playerId=${playerId}`;
-        window.location.href = url;
-    });
-}
+
+
